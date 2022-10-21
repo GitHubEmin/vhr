4e66fad8edec45571fb91fc7ad3dd48739378e1c
Emin <yeminx@outlook.com>
2022-10-21 16:56:54 +0800

    切换mysql8数据库修改配置

--- src/main/java/org/screw/vhr/controller/config/SystemConfigController.java
+++ src/main/java/org/screw/vhr/controller/config/SystemConfigController.java
@@ -3,7 +3,6 @@ package org.screw.vhr.controller.config;
 import java.util.List;
 import org.screw.vhr.model.Menu;
 import org.screw.vhr.service.MenuService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/org/screw/vhr/exception/GlobalExceptionHandler.java
+++ src/main/java/org/screw/vhr/exception/GlobalExceptionHandler.java
@@ -1,26 +1,22 @@
 package org.screw.vhr.exception;
 
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import org.screw.vhr.model.RespBean;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
- * @ClassName GlobalExceptionHandler
- * @Description 全局异常处理
- * @Author Emin
- * @Date 2022/9/27 17:18
- * @Version 1.0
- **/
+ * @author Emin
+ * @date 2022/9/22 19:03
+ */
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(SQLException.class)
     public RespBean sqlException(SQLException e) {
-        if (e instanceof MySQLIntegrityConstraintViolationException) {
+        if (e instanceof SQLIntegrityConstraintViolationException) {
             return RespBean.error("该数据有关联数据，操作失败！");
         }
         return RespBean.error("数据库异常，操作失败！");
